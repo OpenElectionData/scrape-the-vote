7b4ff1b2f1b24897e920810c672d6aa0a846b9a3
Cathy Deng <cathyd125@gmail.com>
2015-05-01 16:02:20 -0500

    update dispatcher

--- stv/main.py
+++ stv/main.py
@@ -14,21 +14,13 @@ def dispatch():
     parser = ArgumentParser(description="")
     parser_subparsers = parser.add_subparsers()
     sub_init = parser_subparsers.add_parser('init')
-    sub_urlgrab = parser_subparsers.add_parser('urlgrab')
-    sub_process = parser_subparsers.add_parser('process')
-    sub_upload = parser_subparsers.add_parser('upload')
+    sub_scrape = parser_subparsers.add_parser('scrape')
 
     sub_init.add_argument(dest='scrapername', help='the name of the scraper to initialize')
     sub_init.set_defaults(func=init)
 
-    sub_urlgrab.add_argument(dest='scrapername', help='the name of the scraper to run')
-    sub_urlgrab.set_defaults(func=urlgrab)
-
-    sub_process.add_argument(dest='electionid', help='the election id')
-    sub_process.set_defaults(func=process)
-
-    sub_upload.add_argument(dest='electionid', help='the election id')
-    sub_upload.set_defaults(func=upload)
+    sub_scrape.add_argument(dest='scrapername', help='the name of the scraper to run')
+    sub_scrape.set_defaults(func=scrape)
 
     args = parser.parse_args()
     args.func(args)
@@ -40,22 +32,10 @@ def init(args) :
     else:
         print('Please specify a scraper name')
 
-def urlgrab(args) :
+def scrape(args) :
     if args.scrapername:
         module = __import__('stv.%s' % args.scrapername, globals(), locals(), ['Scraper'])
         scraper = getattr(module, 'Scraper')()
         scraper.scrape()
     else:
         print('Please specify a scraper name')
-
-def process(args) :
-    if args.electionid:
-        print('process', args.electionid)
-    else:
-        print('Please specify an election id')
-
-def upload(args) :
-    if args.electionid:
-        print('upload', args.electionid)
-    else:
-        print('Please specify an election id')
