91c4b83c0dfd4280e7e7d2450f6b5fa025ab5580
Eric van Zanten <eric.vanzanten@gmail.com>
2015-06-15 16:19:27 -0500

    Added the rest of Georgia scraper. Added exception handling for uploader

--- stv/georgia_election/__init__.py
+++ stv/georgia_election/__init__.py
@@ -9,10 +9,10 @@ import os
 class Scraper(scrapelib.Scraper):
     def __init__(   self,
                     raise_errors=True,
-                    requests_per_minute=30,
+                    requests_per_minute=60,
                     follow_robots=True,
                     retry_attempts=3,
-                    retry_wait_seconds=2,
+                    retry_wait_seconds=5,
                     header_func=None,
                     url_pattern=None,
                     string_on_page=None ):
@@ -22,16 +22,15 @@ class Scraper(scrapelib.Scraper):
                                                 retry_attempts=retry_attempts,
                                                 retry_wait_seconds=retry_wait_seconds,
                                                 header_func=header_func )
+        
         self.base_url = "http://results2014.cec.gov.ge"
         self.election_id = 3
         self.dc_project = 'Georgia Mayoral'
      
     def _lxmlize(self, url, payload=None):
 
-        if payload :
-            entry = self.post(url, payload)
-        else :
-            entry = self.get(url)
+        _, entry = self.urlretrieve(url)
+        
         page = lxml.html.fromstring(entry.content)
         page.make_links_absolute(url)
         return page
@@ -69,5 +68,29 @@ class Scraper(scrapelib.Scraper):
                         img_metadata['hierarchy'] = u'/%s/%s' % (meri_name, oqmi_number)
                         
                         yield (image_url, img_metadata, None)
+        
+        the_rest_start_url = 'http://results2014.cec.gov.ge/proporciuli.html'
+        root_tree = self._lxmlize(the_rest_start_url)
 
+        olq_names = root_tree.xpath("//table[@id='table36']//a/text()")
+        olq_urls = root_tree.xpath("//table[@id='table36']//a/@href")
 
+        for olq_name, olq_url in zip(olq_names, olq_urls):
+            if olq_url != the_rest_start_url:
+                olq_name = olq_name.split(' ', 1)[1]
+
+                olq_tree = self._lxmlize(olq_url)
+                cells = olq_tree.xpath("//td[@bgcolor='#EFEFEF']")
+
+                for cell in cells:
+                    if cell.find('a') is not None:
+                        image_link = cell.find('a')
+                        image_page_url = image_link.attrib['href']
+                        olq_number = image_page_url.rsplit('_', 1)[1].split('.')[0]
+                    
+                        image_page = self._lxmlize(image_page_url)
+                        image_url = image_page.xpath('//img/@src')[0]
+
+                        img_metadata['hierarchy'] = u'/%s/%s' % (olq_name, olq_number)
+                        
+                        yield (image_url, img_metadata, None)
--- stv/main.py
+++ stv/main.py
@@ -13,7 +13,9 @@ import sqlite3
 import hashlib
 import subprocess
 import time
-
+import requests
+import socket
+from datetime import datetime
 
 def dispatch():
 
@@ -142,14 +144,28 @@ def crawl(args) :
 
     for image in images:
         con = sqlite3.connect('documents.db')
-
-        with con:
-            cur = con.cursor()
-            insert_str = u'INSERT INTO temp_documents \
-                           (url,hierarchy,post_data,is_seen) \
-                           VALUES (?,?,?,?);'
-            cur.execute(insert_str,(image[0],image[1]['hierarchy'],image[2],False))
-            con.commit()
+        
+        insert_str = u'INSERT INTO temp_documents \
+                       (url,hierarchy,post_data,is_seen) \
+                       VALUES (?,?,?,?);'
+        
+        try:
+            with con:
+                con.execute(insert_str,(image[0],image[1]['hierarchy'],image[2],False))
+        except sqlite3.OperationalError as e:
+            time.sleep(1)
+            if 'database is locked' in str(e):
+                print(e)
+                i = 0
+                while i < 10:
+                    try:
+                        with con:
+                            con.execute(insert_str,(image[0],image[1]['hierarchy'],image[2],False))
+                        break
+                    except sqlite3.OperationalError as e:
+                        print(e)
+                        i += 1
+                        continue
 
 
 def upload(args) :
@@ -173,81 +189,90 @@ def upload(args) :
     if scraper.election_id:
         election_id = scraper.election_id
 
-    con = sqlite3.connect('documents.db')
 
     while True:
-        with con:
-            cur = con.cursor()
-            image_q = 'SELECT * FROM temp_documents where is_seen=0'
-            cur.execute(image_q)
-            image = cur.fetchone()
+        con = sqlite3.connect('documents.db')
+        
+        cur = con.cursor()
+        image_q = 'SELECT * FROM temp_documents where is_seen=0'
+        cur.execute(image_q)
+        image = cur.fetchone()
+        con.close()
         
         if image:
-            image_temp_id = image[0]
-            file_dl_error = False
-            head, tail = os.path.split(image[1])
+            image_temp_id, head_tail, hierarchy, post_data = image[:4] 
+            
+            head, tail = os.path.split(head_tail)
 
             if os.path.exists(img_dir+tail):
                 os.remove(img_dir+tail)
-
-            if not image[3]: # no post data required
-                try:
-                    r = scraper.get(image[1], stream=True)
-                except:
-                    file_dl_error = True
-                    print("*ERROR* downloading file from %s" %image[1])
-            else: # post data required
-                try:
-                    r = scraper.post(image[1], data=image[3], stream=True)
-                except:
-                    file_dl_error = True
-                    print("*ERROR* downloading file from %s" %image[1])
+            
+            print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'), image[1])
 
             hasher = hashlib.sha1()
             file_hash = ''
             hierarchy = image[2]
             timestamp_server = ''
             timestamp_local = ''
-            if r.status_code == 200 and not file_dl_error: #what to do for other status codes?
-                with open(img_dir+tail, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=1024):
-                        hasher.update(chunk)
-                        f.write(chunk)
-                        f.flush()
-                file_hash = hasher.hexdigest()
-                timestamp_server = r.headers['last-modified'] if 'last-modified' in r.headers else ''
-                timestamp_local = r.headers['date'] if 'date' in r.headers else ''
+            
+            req_kwargs = {}
+
+            if post_data:
+                req_kwargs = {'method': 'POST', body: post_data}
+            with open(img_dir+tail, 'wb') as f:
+                try:
+                    _, r = scraper.urlretrieve(image[1], **req_kwargs)
+                except (requests.HTTPError, socket.error):
+                    i = 0
+                    while i < 10:
+                        time.sleep(1)
+                        try:
+                            _, r = scraper.urlretrieve(image[1], **req_kwargs)
+                            break
+                        except (requests.HTTPError, socket.error):
+                            i += 1
+                            print('retrying %s' % image[1])
+                            continue
+                f.write(r.content)
+                hasher.update(r.content)
+            file_hash = hasher.hexdigest()
+            timestamp_server = r.headers['last-modified'] if 'last-modified' in r.headers else ''
+            timestamp_local = r.headers['date'] if 'date' in r.headers else ''
             
             # adding image to documents table
             con = sqlite3.connect('documents.db')
+            
+            insert_str = 'INSERT INTO documents \
+                        (election_id,url,name,file_hash,hierarchy,timestamp_server,timestamp_local) \
+                        VALUES (?,?,?,?,?,?,?);'
+            q_update = 'SELECT * FROM documents where url=? and file_hash!=? and file_hash!=""'
+            q_duplicate = 'SELECT * FROM documents where url=? and file_hash=?'
+            q_mark_seen = 'UPDATE temp_documents SET is_seen=? where id=?'
 
-            with con:
-                cur = con.cursor()
-                insert_str = 'INSERT INTO documents \
-                            (election_id,url,name,file_hash,hierarchy,timestamp_server,timestamp_local) \
-                            VALUES (?,?,?,?,?,?,?);'
-                q_update = 'SELECT * FROM documents where url=? and file_hash!=? and file_hash!=""'
-                q_duplicate = 'SELECT * FROM documents where url=? and file_hash=?'
-                q_mark_seen = 'UPDATE temp_documents SET is_seen=? where id=?'
-                cur.execute(q_mark_seen,(1, image_temp_id))
-                cur.execute(q_update,(image[1],file_hash))
-                is_update = cur.fetchone()
-                cur.execute(q_duplicate,(image[1],file_hash))
-                is_duplicate = cur.fetchone()
-                cur.execute(insert_str,(election_id,image[1],tail,file_hash,hierarchy,timestamp_server,timestamp_local))
-                con.commit()
+            cur = con.cursor()
+            cur.execute(q_mark_seen,(1, image_temp_id))
+            cur.execute(q_update,(image[1],file_hash))
+            is_update = cur.fetchone()
+            cur.execute(q_duplicate,(image[1],file_hash))
+            is_duplicate = cur.fetchone()
+            cur.execute(insert_str,(election_id,image[1],tail,file_hash,hierarchy,timestamp_server,timestamp_local))
+            con.commit()
 
-                if not is_duplicate and not file_dl_error:
-                    # do something here if image is an update of an image we've already seen
+            con.close()
 
-                    metadata={}
-                    # uploading image to document cloud
-                    metadata['hierarchy'] = hierarchy.encode('utf-8')
-                    metadata['election_id'] = str(election_id)
-                    metadata['timestamp_local'] = timestamp_local
-                    metadata['timestamp_server'] = timestamp_server
-                    obj = client.documents.upload(img_dir+tail, access='public', project=str(project.id), data=metadata)
+            if not is_duplicate:
+                # do something here if image is an update of an image we've already seen
 
-            os.remove(img_dir+tail)
+                metadata={}
+                # uploading image to document cloud
+                metadata['hierarchy'] = hierarchy.encode('utf-8')
+                metadata['election_id'] = str(election_id)
+                metadata['timestamp_local'] = timestamp_local
+                metadata['timestamp_server'] = timestamp_server
+                obj = client.documents.upload(img_dir+tail, access='public', project=str(project.id), data=metadata)
+            try:
+                os.remove(img_dir+tail)
+            except OSError:
+                pass
         else:
             time.sleep(5)
